785b330f26461f60bf4cac5d041bbb6fa7ab3567
ocbj <5monkeys1email@gmail.com>
2023-04-04 11:40:10 +0100

    [ocbj-engine] Update ImConfig to use uint32_t for ImTextureID

--- imconfig.h
+++ imconfig.h
@@ -15,6 +15,8 @@
 #pragma once
 #include <glm/glm.hpp>
 
+#define ImTextureID uint32_t
+
 //---- Define assertion handler. Defaults to calling assert().
 // If your macro uses multiple statements, make sure is enclosed in a 'do { .. } while (0)' block so it can be used as a single statement.
 //#define IM_ASSERT(_EXPR)  MyAssert(_EXPR)
